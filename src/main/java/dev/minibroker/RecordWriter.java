32e5719d6240fd0db8f9dd0519cf084670093b4c
junseokhub <github.yam117@passinbox.com>
2026-05-04 12:49:39 +0900

    feat: add RecordWriter with binary append

---
+++ src/main/java/dev/minibroker/RecordWriter.java
@@ -0,0 +1,41 @@
+package dev.minibroker;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class RecordWriter {
+
+    private final FileChannel channel;
+
+    public RecordWriter(Path path) throws IOException {
+        this.channel = FileChannel.open(path,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.APPEND,
+                StandardOpenOption.CREATE);
+    }
+
+    // kafka 스타일 바이너리 append
+    public void append(Record record) throws IOException {
+        // 고정 바이트 총 합
+        int size = 22 + record.key().length + record.value().length;
+
+        ByteBuffer buffer = ByteBuffer.allocate(size); // 새로운 공간
+        buffer.putShort((short) 0xCAFE);
+        buffer.putLong(record.offset());
+        buffer.putLong(record.timestamp());
+        buffer.putShort((short) record.key().length);
+        buffer.put(record.key());
+        buffer.putShort((short) record.value().length);
+        buffer.put(record.value());
+
+        buffer.flip();
+        channel.write(buffer);
+    }
+
+    public void close() throws IOException {
+        channel.close();
+    }
+}
