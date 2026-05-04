766f07802c12449b0869aa0c8a681b6e67351926
junseokhub <github.yam117@passinbox.com>
2026-05-04 12:55:53 +0900

    feat: add RecordWriter with binary append, handle null key/value

--- src/main/java/dev/minibroker/RecordWriter.java
+++ src/main/java/dev/minibroker/RecordWriter.java
@@ -20,16 +20,30 @@ public class RecordWriter {
     // kafka 스타일 바이너리 append
     public void append(Record record) throws IOException {
         // 고정 바이트 총 합
-        int size = 22 + record.key().length + record.value().length;
+        int keyLength = (record.key() == null) ? 0 : record.key().length;
+        int valueLength = (record.value() == null) ? 0 : record.value().length;
+
+        int size = 22 + keyLength + valueLength;
+
+
 
         ByteBuffer buffer = ByteBuffer.allocate(size); // 새로운 공간
         buffer.putShort((short) 0xCAFE);
         buffer.putLong(record.offset());
         buffer.putLong(record.timestamp());
-        buffer.putShort((short) record.key().length);
-        buffer.put(record.key());
-        buffer.putShort((short) record.value().length);
-        buffer.put(record.value());
+
+        if (record.key() == null) {
+            buffer.putShort((short) -1);
+        } else {
+            buffer.putShort((short) record.key().length);
+            buffer.put(record.key());
+        }
+        if (record.value() == null) {
+            buffer.putShort((short) -1);
+        } else {
+            buffer.putShort((short) record.value().length);
+            buffer.put(record.value());
+        }
 
         buffer.flip();
         channel.write(buffer);
