d383687377e72c07edbd876156ac8ba925d3e8d0
junseokhub <github.yam117@passinbox.com>
2026-05-11 16:32:57 +0900

    feat: add IndexReader with findPosition

--- src/main/java/dev/minibroker/IndexWriter.java
+++ src/main/java/dev/minibroker/IndexWriter.java
@@ -34,6 +34,27 @@ public class IndexWriter implements AutoCloseable {
         count++;
     }
 
+    public long findPosition(long targetOffset) throws IOException {
+        long result = 0; // 못 찾으면 파일 처음부터 읽기
+        channel.position(0); // 항상 처음부터 탐색
+
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+        while (channel.read(buffer) == 16) {
+            buffer.flip();
+            long indexOffset   = buffer.getLong();
+            long indexPosition = buffer.getLong();
+            buffer.clear();
+
+            if (indexOffset <= targetOffset) {
+                result = indexPosition;
+            } else {
+                // index가 정렬된 순서로 저장되어있으니깐 더 읽을 필요가 없음
+                break;
+            }
+        }
+        return result;
+    }
+
     @Override
     public void close() throws IOException {
         channel.close();
