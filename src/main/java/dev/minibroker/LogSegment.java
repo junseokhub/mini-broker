3dde73172743a44380ffd532e8be51326aa92dbf
junseokhub <github.yam117@passinbox.com>
2026-05-11 16:38:58 +0900

    feat: add logSegment combining RecordWriter and IndexWriter

---
+++ src/main/java/dev/minibroker/LogSegment.java
@@ -0,0 +1,26 @@
+package dev.minibroker;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class LogSegment implements AutoCloseable {
+
+    private final RecordWriter recordWriter;
+    private final IndexWriter indexWriter;
+
+    public LogSegment(Path logPath, Path indexPath, int indexInterval) throws IOException {
+        this.recordWriter = new RecordWriter(logPath);
+        this.indexWriter = new IndexWriter(indexPath, indexInterval);
+    }
+
+    public void append(Record record) throws IOException {
+        long position = recordWriter.append(record);
+        indexWriter.maybeIndex(record.offset(), position);
+    }
+
+    @Override
+    public void close() throws IOException {
+        recordWriter.close();
+        indexWriter.close();
+    }
+}
